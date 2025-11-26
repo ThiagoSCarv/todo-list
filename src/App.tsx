cd660c7307584dd12f243b555289966a674a0671
ThiagoSCarv <thiagocarv4@gmail.com>
2025-11-26 18:04:58 -0300

    feature:icon_component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Text } from "./components/text";
+import Icon from "./components/icon";
+import TrashIcon from "./assets/icons/Trash-Regular.svg?react";
+import SpinIcon from "./assets/icons/Spinner.svg?react";
+
 function App() {
 	return (
 		<div className="text-pink-base">
-			<h1>Hello World</h1>
+			<Text className="text-pink-base" variant="body-sm">
+				Hello World!!
+			</Text>
+			<Text className="text-gray-400" variant="body-md-regular">
+				Hello World!!
+			</Text>
+			<Text className="text-gray-200" variant="body-md-semibold">
+				Hello World!!
+			</Text>
+			<div>
+				<Icon svg={TrashIcon} className="fill-pink-dark" />
+				<Icon svg={SpinIcon} className="fill-green-dark" />
+			</div>
 		</div>
 	);
 }
