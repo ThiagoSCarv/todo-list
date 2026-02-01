3b2024778e6d51365d3e3eda36f14ac21473f9a3
Thiago <thiagocarv4@gmail.com>
2026-02-01 11:10:05 -0300

    feat: adicionando componente button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Text } from "./components/text";
 import Icon from "./components/icon";
 import TrashIcon from "./assets/icons/Trash-Regular.svg?react";
 import SpinIcon from "./assets/icons/Spinner.svg?react";
+import PlusIcon from "./assets/icons/Plus-Regular.svg?react";
+import Button from "./components/button";
 import Badge from "./components/badge";
 
 function App() {
@@ -24,6 +26,10 @@ function App() {
 			<div>
 				<Badge variant="secondary">5</Badge>
 				<Badge variant="primary">2 de 5</Badge>
+			</div>
+
+			<div>
+				<Button icon={PlusIcon}>Nova Tarefa</Button>
 			</div>
 		</div>
 	);
